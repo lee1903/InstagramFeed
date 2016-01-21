583b5228e35ce921d8447029f3598c056dfd9142
Lainie Wright <lainiewright@gmail.com>
2016-01-21 17:04:29 -0500

    Milestone 3

---
+++ InstagramFeed/PhotosCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCell.swift
+//  InstagramFeed
+//
+//  Created by Lainie Wright on 1/21/16.
+//  Copyright © 2016 brianlee. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+
+}
--- InstagramFeed/PhotosViewController.swift
+++ InstagramFeed/PhotosViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import AFNetworking
 
-class ViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var instaTableView: UITableView!
     
     var feed: [NSDictionary]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        instaTableView.delegate = self
+        instaTableView.dataSource = self
         
         let clientId = "e05c462ebd86446ea48a5af73769b602"
         let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
@@ -30,13 +36,39 @@ class ViewController: UIViewController {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
-                            self.feed = responseDictionary["data"] as! NSDictionary
+                            self.feed = responseDictionary["data"] as! [NSDictionary]
+                            self.instaTableView.reloadData()
                     }
                 }
         });
         task.resume()
         
+        instaTableView.rowHeight = 320;
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let feed = feed {
+            return feed.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = instaTableView.dequeueReusableCellWithIdentifier("PhotosCell", forIndexPath: indexPath) as! PhotosCell
+        
+        let post = feed![indexPath.row]
+        let images = post["images"] as! NSDictionary
+        let stdRes = images["standard_resolution"] as! NSDictionary
+        
+        let imageUrl = stdRes["url"] as! String
+        let url = NSURL(string: imageUrl)
+        
+        cell.photoImageView.setImageWithURL(url!)
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
